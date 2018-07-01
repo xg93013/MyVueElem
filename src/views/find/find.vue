eee0581be789662c6e4eb49e94bbf19f017585c4
xg93013 <963936199@qq.com>
2018-07-01 23:52:43 +0800

    add new

--- src/views/find/find.vue
+++ src/views/find/find.vue
@@ -82,7 +82,7 @@
               </div>
             </div>
             <div class="find-more">
-              <router-link to="/find/findList" tag="span">查看更多</router-link>
+              <router-link to="/find/findList" tag="span">查看更多new</router-link>
             </div>
           </div>
         </section>
